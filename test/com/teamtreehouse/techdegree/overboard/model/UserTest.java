6106fd304444b3db5d04cc8e20d7faee2da63ff0
Jorge Otero <oterogalvis@gmail.com>
2017-04-19 23:45:46 -0400

    Test user getReputation()

--- test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ test/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,7 +1,11 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
+import com.teamtreehouse.techdegree.overboard.exc.AnswerAcceptanceException;
+import com.teamtreehouse.techdegree.overboard.exc.VotingException;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
@@ -13,27 +17,98 @@ public class UserTest {
     private Board boardA;
     private User userA;
     private User userB;
-    private User userC;
     private Question questionA;
     private Answer answerB;
-    private Answer answerC;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
         boardA = new Board("Test");
         userA = new User(boardA, "userA");
         userB = new User(boardA, "userB");
-        userC = new User(boardA, "userC");
         questionA = userA.askQuestion("questionA");
         answerB = userB.answerQuestion(questionA, "answerB");
-        answerC = userB.answerQuestion(questionA, "answerC");
     }
 
     @Test
-    public void reputationGoUp5WhenUpvoteQuestion() throws Exception {
+    public void reputationGoUp5WhenQuestionUpvoted() throws Exception {
         userB.upVote(questionA);
 
         assertEquals("Verify that the questioner’s reputation goes up by 5 points if their question is upvoted.",
                 5, userA.getReputation());
+    }
+
+    @Test
+    public void reputationGoUp10WhenAnswerUpvoted() throws Exception {
+        userA.upVote(answerB);
+
+        assertEquals("Verify that the answerer’s reputation goes up by 10 points if their answer is upvoted.",
+                10, userB.getReputation());
+    }
+
+    @Test
+    public void reputationGoUp15WhenAnswerAccepted() throws Exception {
+        userA.acceptAnswer(answerB);
+
+        assertEquals("Verify that having an answer accepted gives the answerer a 15 point reputation boost.",
+                15, userB.getReputation());
+    }
+
+    @Test
+    public void reputationGoDown1WhenAnswerDownvoted() throws Exception {
+        userA.downVote(answerB);
+
+        assertEquals("Verify down-voting costs 1 point",
+                -1, userB.getReputation());
+    }
+
+    @Test
+    public void reputationDoNotChangeWhenQuestionDownvoted() throws Exception {
+        userB.downVote(questionA);
+
+        assertEquals("Verify down-voting of questions affects nothing.",
+                0, userA.getReputation());
+    }
+
+    @Test
+    public void voteUpQuestionByQuestioner() throws Exception {
+        expectedException.expect(VotingException.class);
+        expectedException.expectMessage("You cannot vote for yourself!");
+
+        userA.upVote(questionA);
+    }
+
+    @Test
+    public void voteDownQuestionByQuestioner() throws Exception {
+        expectedException.expect(VotingException.class);
+        expectedException.expectMessage("You cannot vote for yourself!");
+
+        userA.downVote(questionA);
+    }
+
+    @Test
+    public void voteUpAnswerByAnswerer() throws Exception {
+        expectedException.expect(VotingException.class);
+        expectedException.expectMessage("You cannot vote for yourself!");
+
+        userB.upVote(answerB);
+    }
+
+    @Test
+    public void voteDownAnswerByAnswerer() throws Exception {
+        expectedException.expect(VotingException.class);
+        expectedException.expectMessage("You cannot vote for yourself!");
+
+        userB.downVote(answerB);
+    }
+
+    @Test
+    public void acceptAnswerFromAnotherAuthorQuestion() throws Exception {
+        expectedException.expect(AnswerAcceptanceException.class);
+        expectedException.expectMessage(String.format("Only %s can accept this answer as it is their question", userA.getName()));
+
+        userB.acceptAnswer(answerB);
     }
 }
